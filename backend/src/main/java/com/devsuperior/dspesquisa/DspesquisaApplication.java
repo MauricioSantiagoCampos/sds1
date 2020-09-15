d06a7773eb2bcd13e881d2569783fbd4b3b22cd7
Mauricio Santiago Campos <mauricio_santiago_20@hotmail.com>
2020-09-15 09:54:27 -0300

    Entities, Repositories

--- backend/src/main/java/com/devsuperior/dspesquisa/DspesquisaApplication.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/DspesquisaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OsworksApi1Application {
+public class DspesquisaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OsworksApi1Application.class, args);
+		SpringApplication.run(DspesquisaApplication.class, args);
 	}
 
 }
--- backend/src/test/java/com/devsuperior/dspesquisa/DspesquisaApplicationTests.java
+++ backend/src/test/java/com/devsuperior/dspesquisa/DspesquisaApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OsworksApi1ApplicationTests {
+class DspesquisaApplicationTests {
 
 	@Test
 	void contextLoads() {
